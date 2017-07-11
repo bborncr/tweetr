4ff4bf42624ef812b447b04e3d067f7f2d2030f8
bborncr <bentley@crcibernetica.com>
2017-07-11 15:24:04 +0000

    make counter red when invalid

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,13 +7,20 @@ $( document ).ready(function() {
   // create eventListeners
   $('.primary-input').on('focus', function(event){
     isFocussed = true;
+    counter = $(this).val().length;
     $('.counter').text(counter);
   });
   $('.primary-input').on('keyup', function(event){
     if(isFocussed){
       counter = $(this).val().length;
+      if(counter <= 140){
+        $('.counter').css('color','black');
+        $('.counter').text(counter);
+      } else {
+        $('.counter').css('color','red');
+        counter = 140 - $(this).val().length;
+      }
       $('.counter').text(counter);
-
     }
   });
 
