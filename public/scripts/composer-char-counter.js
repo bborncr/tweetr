7e7d7459590d0523c1df630ccff3587e3b5fe15a
bborncr <bentley@crcibernetica.com>
2017-07-11 16:36:21 +0000

    some refactoring of counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,28 +1,21 @@
-
 $( document ).ready(function() {
   let counter = 0;
-  let isFocussed = false;
-  console.log('Document is ready...');
+
+  function updateCounter(event){
+    counter = $(this).val().length;
+    if(counter <= 140){
+      $('.counter').css('color','black');
+      $('.counter').text(counter);
+    } else {
+      $('.counter').css('color','red');
+      counter = 140 - $(this).val().length;
+    }
+    $('.counter').text(counter);
+  }
 
   // create eventListeners
-  $('.primary-input').on('focus', function(event){
-    isFocussed = true;
-    counter = $(this).val().length;
-    $('.counter').text(counter);
-  });
-  $('.primary-input').on('keyup', function(event){
-    if(isFocussed){
-      counter = $(this).val().length;
-      if(counter <= 140){
-        $('.counter').css('color','black');
-        $('.counter').text(counter);
-      } else {
-        $('.counter').css('color','red');
-        counter = 140 - $(this).val().length;
-      }
-      $('.counter').text(counter);
-    }
-  });
+  $('.primary-input').on('focus', updateCounter);
+  $('.primary-input').on('keyup', updateCounter);
 
 });
 
